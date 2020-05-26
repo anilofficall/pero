41c7dfb7b5d105445841b43c11bd42282e4b097a
Glitch (hello-express) <none>
2020-05-26 14:42:37 +0000

    🏜🎈 Checkpoint ./komutlar/avatar.js:11195635/1716

--- komutlar/avatar.js
+++ komutlar/avatar.js
@@ -1,22 +1,21 @@
-const Discord = require("discord.js");
-exports.run = function(client, message, args) {
-let user;  
-if (message.mentions.users.first()) {
-user = message.mentions.users.first();
-    } else {
-        user = message.author;}
-return message.channel.send(new Discord.MessageEmbed()
-.setColor(`RANDOM`)
-.setDescription(`**${user.tag}** İşte Avatarın;`)
-.setImage(user.avatarURL()))
-};  
-
+const Discord = require('discord.js')
+exports.run = (client, message) => {
+const embed = message.mentions.users.first()
+let user;
+if (message.mentions.users.first())  {user = message.mentions.users.first();}
+ else {user = message.author;}
+return message.channel.send(new Discord.MessageEmbed())
+.setFooter('İşte Avatarın;')
+.setColor(`RED`)
+                            
+}
 exports.conf = {
-  enabled: true, 
-  guildOnly: true, 
-  aliases: [],
-  permLevel: 0 
-};
-exports.help = {
-  name: 'avatar'
-};
+    enabled: false,
+    guildOnly: false,
+    aliases: [],
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'avatar'
+  };
