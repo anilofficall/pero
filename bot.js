94ee5974ace2b22bf5edf93e90568a5be3853f2a
Glitch (cw-deneme-v12) <none>
2020-05-26 16:17:12 +0000

    Rewound to commit 77b3be82a58718eff3029695f2910000ca8afa06 + 0 edits

--- bot.js
+++ bot.js
@@ -1,43 +1,125 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const express = require("express");
+const discord = require('discord.js');
+const fs = require('fs');
+const http = require('http');
+const express = require('express');
+const ayarlar = require('./ayarlar.json');
 const app = express();
 
-const fs = require("fs");
+//UPTİME İÇİN
 
-//Uptime için__________________________________________________________________
-app.get("/", (req, res) => {
-  res.send("HELO");
+app.get("/", (request, response) => {
+  console.log("Otomatik Bağlama İşlemi Tamamlandı"); // KANKA BU GLİTCH İÇİN VDS GEÇİRCEKSEN SİL BURAYI
+  response.sendStatus(200);
 });
-app.listen(process.env.PORT);
+app.listen(8000);
+setInterval(() => {
+  http.get(`https://cw-deneme-v12.glitch.me/`);
+}, 120000)
 
-//KOMUT Algılayıcı______________________________________________________________
+//UPTİME İÇİN
+
+//READY.JS
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', async () => {
+   client.appInfo = await client.fetchApplication();
+  setInterval( async () => {
+    client.appInfo = await client.fetchApplication();
+  }, 600);
+  
+ client.user.setActivity(`CodeWork Taner Tolga Tarlacı`, { type:"WATHING" })
+  
+  console.log("CodeWork Akıyor!!")
+});
+
+const log = message => {
+  console.log(` ${message}`);
+};
+require('./util/eventLoader.js')(client);
+//READY.JS
+
 client.commands = new Discord.Collection();
-
-fs.readdir("./komutlar/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    let cmd = require(`./komutlar/${file}`);
-    let cmdFileName = file.split(".")[0];
-    console.log(`Komut Yükleme Çalışıyor: ${cmdFileName}`);
-    client.commands.set(cmd.help.name, cmd);
-  });
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+    if (err) console.error(err);
+    log(`${files.length} komut yüklenecek.`);
+    files.forEach(f => {
+        let props = require(`./komutlar/${f}`);
+        log(`Yüklenen komut: ${props.help.name}.`);
+        client.commands.set(props.help.name, props);
+        props.conf.aliases.forEach(alias => {
+            client.aliases.set(alias, props.help.name);
+        });
+    });
 });
 
-//EVENTS Yükleyici_______________________________________________________________
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach((file) => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    console.log(`Etkinlik Yükleme Çalışıyor: ${eventName}`);
-    client.on(eventName, event.bind(null, client));
-  });
-});
 
-client.on("ready", () => {
-  console.log(`${client.user.tag}! Aktif!`);
-});
 
-client.login(process.env.TOKEN);
+
+client.reload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+           reject(e);
+        }
+    });
+};
+
+client.load = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.set(command, cmd);
+            cmd.conf.aliases.forEach(alias => {
+                client.aliases.set(alias, cmd.help.name);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+
+
+client.unload = command => {
+    return new Promise((resolve, reject) => {
+        try {
+            delete require.cache[require.resolve(`./komutlar/${command}`)];
+            let cmd = require(`./komutlar/${command}`);
+            client.commands.delete(command);
+            client.aliases.forEach((cmd, alias) => {
+                if (cmd === command) client.aliases.delete(alias);
+            });
+            resolve();
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+
+client.elevation = message => {
+    if (!message.guild) {
+        return;
+    }
+    let permlvl = 0;
+    if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+    if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+    if (message.author.id === ayarlar.sahip) permlvl = 4;
+    return permlvl;
+};
+client.login(process.env.TOKEN)
--- events/message.js
+++ events/message.js
@@ -1,15 +1,30 @@
-const prefix = process.env.PREFIX;
+const Discord = require("discord.js");
+const ayarlar = require('../ayarlar.json');
+let talkedRecently = new Set();
 
-module.exports = (client, message) => {
+module.exports = message => {
+  if (talkedRecently.has(message.author.id)) {
+    return;
+  }
+  talkedRecently.add(message.author.id);
+	setTimeout(() => {
+    talkedRecently.delete(message.author.id);
+  }, 2500);
+  let client = message.client;
   if (message.author.bot) return;
-  if (message.channel.type == "dm") return;
+  if (!message.content.startsWith(ayarlar.prefix)) return;
+  let command = message.content.split(' ')[0].slice(ayarlar.prefix.length);
+  let params = message.content.split(' ').slice(1);
+  let perms = client.elevation(message);
+  let cmd;
+  if (client.commands.has(command)) {
+    cmd = client.commands.get(command);
+  } else if (client.aliases.has(command)) {
+    cmd = client.commands.get(client.aliases.get(command));
+  }
+  if (cmd) {
+    if (perms < cmd.conf.permLevel) return;
+    cmd.run(client, message, params, perms);
+  }
 
-  const msgArr = message.content.split(/\s+/g);
-  const command = msgArr[0];
-  const args = msgArr.slice(1);
-
-  if (!command.startsWith(prefix)) return;
-
-  let cmd = client.commands.get(command.slice(prefix.length));
-  if (cmd) cmd.run(client, message, args);
 };
